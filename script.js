283c71bb74bc49a13d417d9c08027c3e5b648bd8
nguHelon <nguhelon@gmail.com>
2022-09-09 22:04:34 +0100

    made the this project responsive, so happy

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menubtn1 = document.querySelector("#header .header .account .fa-bars");
 const ulNavbar = document.querySelector("#header .header ul");
+const li = document.querySelectorAll("#header .header ul li");
 const ulClose = document.querySelector("#header .header ul li .fa-xmark");
 
 menubtn1.addEventListener("click", () => {
@@ -8,4 +9,18 @@ menubtn1.addEventListener("click", () => {
 
 ulClose.addEventListener("click", function () {
     ulNavbar.style.right = "-280px";
+});
+
+li.forEach((list) => {
+    list.addEventListener("click", () => {
+        if (list.classList.contains("active") == false) {
+            li.forEach((list) => {
+                if (list.classList.contains("active") == true) {
+                    list.classList.remove("active");
+                }
+            });
+        }
+        list.classList.add("active");
+        console.log(list);
+    });
 });
